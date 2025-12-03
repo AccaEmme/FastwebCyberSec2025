e90da1e1f67ab0ff7762f76fd876d85d8faf0d8c
H M <AccaEmme@users.noreply.github.com>
2025-12-03 15:12:17 +0100

    Create my_checkup_script.sh

---
+++ 2025-12-03/my_checkup_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Script written by AccaEmme on 03rd dic 2025
+# GitHub: https://github.com/AccaEmme/FastwebCyberSec2025/2025-12-03/
+#
+# how to install:
+# - copy text into my_checkup_script.sh file
+# - run: chmod +x ./my_checkup_script.sh
+#
+# usage:
+# ./my_checkup_script.sh
+
+echo "=== Mini Comandi bash per lo Scenario 3 - a cura di Hermann ==="
+
+echo " :: Modifiche recenti al file /etc/passwd e /etc/shadow"
+stat /etc/passwd
+ls -lh /etc/passwd /etc/shadow
+
+echo " :: List file log più recenti in /var/log"
+ls -lth /var/log | head -20
+
+echo " :: Accessi con successo mediante SSH in /var/log"
+cat /var/log/auth.log | grep -i "Accepted" | grep -i ssh
+
+echo " :: Gli ultimi utenti loggati. Eseguo: lastlog"
+echo "capire se ci sono potenziali anomalie, come utente root loggato o utenti di sistema loggati"
+lastlog
+
+echo " :: Evidenzia gli utenti che hanno accesso a qualsiasi shell"
+echo "ovvero se la shell è utilizzabile per un utente al quale non sarebbe prevista, capire che succede"
+grep -vE "(/usr/sbin/nologin|/bin/false)" /etc/passwd # Mostra utenti con shell diversa da /usr/sbin/nologin o /bin/false
+
+echo " :: Evidenzia gli utenti che hanno uid=0 e quindi di root"
+awk -F: '($3 == 0) {print $1}' /etc/passwd
+
+# mostra il file /etc/passwd per un controllo umano
+cat /etc/passwd
+echo ""
+echo ""
+
+echo " :: Cercare servizi legati a condizioni temporali particolari o eventi"
+cd /etc/systemd/system
+ls *.timer # Cerca servizi con timer
+grep -i "OnCalendar" *.timer # Cerca servizi legati a eventi
+
+
+echo " :: Elenca i file dei servizi presenti"
+ls /etc/systemd/system/*.service
+systemctl status | grep running
